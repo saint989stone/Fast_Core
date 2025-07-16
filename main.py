1a129e75272dd6ef96cb1b7cad329b3ee991bab5
saint989stone <stone_89@bk.ru>
2025-07-17 00:06:23 +0300

    First run FastApi

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get('/', summary='Главная ручка', tags=['Основные ручки'])
+def root():
+    return 'Heloo World'
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
